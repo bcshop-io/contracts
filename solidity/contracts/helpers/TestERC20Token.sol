317d7b7ded192f62a3b2b4bd4ac590670a22ed12
bcshop-io <kondrahanov@mail.ru>
2017-10-31 15:53:17 +0700

    TestERC20DecimalsToken

--- solidity/contracts/helpers/TestERC20Token.sol
+++ solidity/contracts/helpers/TestERC20Token.sol
@@ -5,7 +5,16 @@ import '../ERC20Token.sol';
     Test token with predefined supply
 */
 contract TestERC20Token is ERC20Token {
-    function TestERC20Token(string _name, string _symbol, uint256 _supply, uint8 _decimals)
+    function TestERC20Token(string _name, string _symbol, uint256 _supply)
+        ERC20Token(_name, _symbol, 0)
+    {
+        totalSupply = _supply;
+        balanceOf[msg.sender] = _supply;
+    }
+}
+
+contract TestERC20DecimalsToken is ERC20Token {
+    function TestERC20DecimalsToken(string _name, string _symbol, uint256 _supply, uint8 _decimals)
         ERC20Token(_name, _symbol, _decimals)
     {
         totalSupply = _supply;
